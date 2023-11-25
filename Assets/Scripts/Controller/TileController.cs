d62d2a46f126f005bdeed67daa7883ed8e19e1df
Oicanji <51388779+Oicanji@users.noreply.github.com>
2023-11-25 17:58:53 -0300

    a

--- Assets/Scripts/Controller/TileController.cs
+++ Assets/Scripts/Controller/TileController.cs
@@ -1,31 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TileController : MonoBehaviour
 {
-    private List<GameObject> bricks = new List<GameObject>();
+    public Tilemap __tilemap; // Referência para o Tilemap que contém os tiles
 
     void Start()
     {
-        PopulateBrickList();
+        __tilemap = GetComponent<Tilemap>();
+        GetTilesFromTileset();
     }
-    public void DamageBrick(GameObject brickObject, float damage)
+
+    void GetTilesFromTileset()
     {
-        BrickController brickController = brickObject.GetComponent<BrickController>();
-        if (brickController != null)
+        TileBase[] allTiles = __tilemap.GetTilesBlock(__tilemap.cellBounds); // Obtém todos os tiles do Tilemap
+
+        if (allTiles != null)
         {
-            brickController.TakeDamage(damage);
-            bricks.Remove(brickObject); // Remove o tijolo da lista quando é destruído
+            for (int i = 0; i < allTiles.Length; i++)
+            {
+                Debug.Log("Tile encontrado: " + allTiles[i]);
+            }
         }
     }
 
-    // Método para preencher a lista de tijolos
-    public void PopulateBrickList()
+    // if collision with ball destroy tile
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject[] allBricks = GameObject.FindGameObjectsWithTag("Breakable");
-        bricks.Clear(); // Limpa a lista antes de preenchê-la novamente
-        bricks.AddRange(allBricks);
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Vector3 hitPosition = Vector3.zero;
+            foreach (ContactPoint2D hit in collision.contacts)
+            {
+                //rand numerber in 1 a 10 and < 3 destroy tile
+                if (Random.Range(1, 10) < 3)
+                {
+                    hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                    hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                    __tilemap.SetTile(__tilemap.WorldToCell(hitPosition), null);
+                }
+            }
+        }
     }
-
 }
