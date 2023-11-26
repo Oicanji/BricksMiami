aeb037873b7241219e36e46b326072da07c9ab74
RyanFrag <92130127+RyanFrag@users.noreply.github.com>
2023-11-26 20:58:03 -0300

    Pontos

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -4,50 +4,54 @@ using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public int PlayerLives { get; private set; } = 3; 
-    [SerializeField] float gameTimer = 120.0f; 
+    public int PlayerLives { get; private set; } = 3;
+    [SerializeField] float gameTimer = 120.0f;
     [SerializeField] TextMeshProUGUI gameInfoText;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     private SceneController sceneController;
+    private int score = 0;
+    private float scoreTimer = 2.0f;
 
     void Start()
     {
         sceneController = FindObjectOfType<SceneController>();
-        gameInfoText = FindObjectOfType<TextMeshProUGUI>();
+        gameInfoText = GameObject.Find("UI").GetComponent<TextMeshProUGUI>();
+        scoreText = GameObject.Find("Points").GetComponent<TextMeshProUGUI>();
 
-        if (gameInfoText == null)
+        if (gameInfoText == null || scoreText == null)
         {
-            Debug.LogError("O objeto gameInfoText não está atribuído. Por favor, atribua-o no Inspector.");
+            Debug.LogError("Os objetos gameInfoText e/ou scoreText não estão atribuídos. Por favor, atribua-os no Inspector.");
         }
 
         UpdateGameInfoText();
+        UpdateScoreText();
     }
 
     void Update()
     {
+        // Atualizar o temporizador do jogo
         if (gameTimer > 0)
         {
             gameTimer -= Time.deltaTime;
-
-            int min = Mathf.FloorToInt(gameTimer / 60);
-            int seg = Mathf.FloorToInt(gameTimer % 60);
-            if (gameTimer < 0)
-            {
-                gameTimer = 0;
-            }
-            if (gameInfoText != null)
-            {
-                gameInfoText.text = string.Format("Tempo: {0:00}:{1:00}\nVidas: {2}", min, seg, PlayerLives);
-            }
+            UpdateGameInfoText();
         }
         else
         {
-            GameOver(); 
+            GameOver();
+        }
+
+        // Atualizar o temporizador do score
+        scoreTimer -= Time.deltaTime;
+        if (scoreTimer <= 0)
+        {
+            AdicionarPontos(1); // Adiciona 1 ponto a cada 2 segundos
+            scoreTimer = 2.0f; // Reinicia o temporizador
         }
 
         if (PlayerLives <= 0)
         {
-            GameOver(); 
+            GameOver();
         }
     }
 
@@ -55,6 +59,20 @@ public class GameController : MonoBehaviour
     {
         PlayerLives--;
         UpdateGameInfoText();
+    }
+
+    void AdicionarPontos(int pontos)
+    {
+        score += pontos;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Pontos: " + score;
+        }
     }
 
     void Win()
@@ -74,7 +92,13 @@ public class GameController : MonoBehaviour
     {
         if (gameInfoText != null)
         {
-            gameInfoText.text = string.Format("{0:00}:{1:00}                Vidas: {2}", Mathf.FloorToInt(gameTimer / 60), Mathf.FloorToInt(gameTimer % 60), PlayerLives);
+            int min = Mathf.FloorToInt(gameTimer / 60);
+            int seg = Mathf.FloorToInt(gameTimer % 60);
+            if (gameTimer < 0)
+            {
+                gameTimer = 0;
+            }
+            gameInfoText.text = string.Format("Tempo: {0:00}:{1:00}\nVidas: {2}", min, seg, PlayerLives);
         }
-    } 
+    }
 }
